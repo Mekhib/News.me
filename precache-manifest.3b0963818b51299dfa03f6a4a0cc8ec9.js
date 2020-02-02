c53243c8d723c70984e05f998ec2b02aa1310661
Mekhib <Mekhi19132@gmail.com>
2020-02-02 09:18:46 -0800

    Updates

---
+++ precache-manifest.3b0963818b51299dfa03f6a4a0cc8ec9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b00a6c279e1ad73b544b274fab910b",
+    "url": "/Mekhib/News.me/index.html"
+  },
+  {
+    "revision": "44340b602ffa532980f8",
+    "url": "/Mekhib/News.me/static/css/2.1cc9201e.chunk.css"
+  },
+  {
+    "revision": "da2d0ba0b495a85a559a",
+    "url": "/Mekhib/News.me/static/css/main.9589200c.chunk.css"
+  },
+  {
+    "revision": "44340b602ffa532980f8",
+    "url": "/Mekhib/News.me/static/js/2.23ee21bc.chunk.js"
+  },
+  {
+    "revision": "7fc60fe721fa046663bd312b1d917655",
+    "url": "/Mekhib/News.me/static/js/2.23ee21bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da2d0ba0b495a85a559a",
+    "url": "/Mekhib/News.me/static/js/main.3723be66.chunk.js"
+  },
+  {
+    "revision": "b1abdf7498506c7ad85d",
+    "url": "/Mekhib/News.me/static/js/runtime-main.6cdcd970.js"
+  }
+]);
